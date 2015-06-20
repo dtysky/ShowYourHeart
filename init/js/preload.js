cdb814c3f4bece8151bea654dfd87a57f3c21fcc
dtysky <dtysky@outlook.com>
2015-06-20 19:05:50 +0800

    Init.
    Signed-off-by: dtysky <dtysky@outlook.com>

---
+++ init/js/preload.js
@@ -0,0 +1,18 @@
+var loader = new resLoader({
+     resources : ["../image/atone.jpg","../image/bye.jpg","../image/confess.jpg","../image/end.jpg","../image/first.jpg","../image/grow-up.jpg","../image/inferior.jpg","../image/lonely.jpg","../image/one.jpg","../image/pride.jpg","../image/refuse.jpg","bgm.ogg"],
+     onStart : function(total){
+          console.log('start:'+total);
+     },
+     onProgress : function(current, total){
+          console.log(current+'/'+total);
+          var percent = current/total*100;
+          $('.progressbar').css('width', percent+'%');
+          $('.progresstext .current').text(current);
+          $('.progresstext .total').text(total);
+     },
+     onComplete : function(total){
+     	$('.progress').css('display', 'none');
+     }
+});
+
+loader.start();
